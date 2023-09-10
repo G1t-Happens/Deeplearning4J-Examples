6ba15e62bf228d77c640dff107637a21fe3c14e9
G1t-Happens <anonymous>
2023-09-10 19:34:48 +0200

    normalizing data

--- Deeplearning4J-Examples/src/main/java/org/example/App.java
+++ Deeplearning4J-Examples/src/main/java/org/example/App.java
@@ -6,6 +6,8 @@ import org.datavec.api.split.FileSplit;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
 import org.nd4j.linalg.io.ClassPathResource;
 
 import java.io.IOException;
@@ -30,6 +32,11 @@ public class App {
         //shuffle the dataset to get rid of ordering of classes
         //constant seed of 45 because we always want the same shuffling results
         allDataSetData.shuffle(45);
+
+        //Normalizing(normal distribution) our data -> https://www.baeldung.com/cs/normalize-table-features
+        DataNormalization normalize = new NormalizerStandardize();
+        normalize.fit(allDataSetData); //gathering tatistics about the data like average and standard deviation
+        normalize.transform(allDataSetData); //data is adjusted so that it has a mean of 0 and a standard deviation of 1
     }
 
 }
